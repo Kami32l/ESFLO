d9022b3788f3a350ec33c5b76fd8f0ad4477fbd5
Kami32l <79849248+Kami32l@users.noreply.github.com>
2023-01-08 02:30:13 +0100

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,7 @@
+from flight_search import FlightSearch
+from date_data import DateData
+
+
 class FlightData:
     """
     This class is responsible for structuring the flight data.
@@ -10,43 +14,38 @@ class FlightData:
         """
         Filters received data to only specified contents
         :param results: data to be filtered and formatted
-
-        :return formated_data: returns formatted data
-        :return link: returns link
+        :return formatted_data: formatted messages in a list
+        :return link: flight links list
         """
         formatted_data = []
         links = []
 
         for result in results['data']:
-            try:
-                city = result['cityTo']
-                price = result['price']
-                departure = result['utc_departure'] + " UCT"
-                arrival = result['utc_arrival']
-                distance = result['distance']
-                stops_count = int(result['pnr_count'])-1
-                seats = result['availability']['seats']
-                link = result['deep_link']
 
-                # print(result)
-                # message = print(f"Flight for {city}")
-                #             print("Price: ", price, "€")
-                #             print("Departure: ", departure)
-                #             print("Arrival: ", arrival)
-                #             print("Distance: ", distance, "km")
-                #             print("Route stops: ", stops_count)
-                #             print("Seat availability: ", seats)
-                #             print("Link: ", link)
+            city = result['cityTo']
+            dep_city = result['cityFrom']
+            price = result['price']
+            departure = result['utc_departure'] + " UCT"
+            arrival = result['utc_arrival']
+            distance = result['distance']
+            stops_count = int(result['pnr_count'])-1
+            seats = result['availability']['seats']
+            link = result['deep_link']
 
-                message_text = self.format_message(city, price, departure, arrival, distance, stops_count, seats, link)
+            # print(result)
+            # message = print(f"Flight for {city}")
+            #             print("Price: ", price, "€")
+            #             print("Departure: ", departure)
+            #             print("Arrival: ", arrival)
+            #             print("Distance: ", distance, "km")
+            #             print("Route stops: ", stops_count)
+            #             print("Seat availability: ", seats)
+            #             print("Link: ", link)
 
-                formatted_data.append(message_text)
-                links.append(link)
+            message_text = self.format_message(city, dep_city, price, departure, arrival, distance, stops_count, seats, link)
 
-            except IndexError:
-                print(f"No flights for {city}")
-
-        print("formated_data", formatted_data)
+            formatted_data.append(message_text)
+            links.append(link)
 
         try:
             return formatted_data, links
@@ -55,20 +54,21 @@ class FlightData:
             links = "Link for flight not found"
             return formatted_data, links
 
-    def format_message(self, city, price, departure, arrival, distance, stops_count, seats, link):
+    def format_message(self, city, dep_city, price, departure, arrival, distance, stops_count, seats, link):
         """
-        Formats received data into string text message used for SMS notification (Twilio API)
-        :param city:
-        :param price:
-        :param departure:
-        :param arrival:
-        :param distance:
-        :param stops_count:
-        :param seats:
-        :param link:
-        :return:
+        Formats received data into string text message.
+        :param city: city of arrival
+        :param dep_city: city of departure
+        :param price: max price
+        :param departure: departure time
+        :param arrival: arrival time
+        :param distance: distance of travel
+        :param stops_count: number of times you need to change a plane
+        :param seats: number of seats available
+        :param link: link to an offer
+        :return: message text
         """
-        message_text = f"Lot do {city}\nCena: {price}€\n" \
+        message_text = f"Lot do {city} z {dep_city}\nCena: {price}€\n" \
                        f"Odlot: {departure}\nPrzylot: {arrival}\n" \
                        f"Dystans: {distance}km\nMiędzylądowania: {stops_count}\n" \
                        f"Dostępność miejsc: {seats}\n" \
@@ -80,3 +80,64 @@ class FlightData:
         # f"Seat availability: {seats}\n" \
         # f"Link: {link}"
         return message_text
+
+    def filter_from_database(self, to_filter):
+        """
+        Prepare data to flight search and search for a flight.
+        :param to_filter: data to filter
+        :return results:
+        """
+
+        # iterates through filtered data
+        status_code = 1
+        results = []
+        for index, row in to_filter.iterrows():
+            # print(row)
+            # print(row["User"], row["MaxPrice"])
+
+            city = row['City']
+            fly_to = row['IATA']
+            fly_from = row['Dep_IATA']
+
+            d = DateData(row['Days_range'])
+            date_from = d.today_date
+
+            date_to = row['Next_date']
+            # print("Next_date:", next_date)
+            price_to = row['MaxPrice']
+            adults = row['People']
+
+            # checks flights
+            found_flight, status_code = FlightSearch().check_flights(fly_from=fly_from, fly_to=fly_to, date_from=date_from,
+                                                        date_to=date_to, price_to=price_to, adults=adults)
+            if status_code == 0:
+                return results, status_code
+
+            results.append(found_flight)
+
+        print("Results loc flight_data.py filter_from_database():", results)
+        return results, status_code
+
+    def filter_from_user_input(self, city, fly_to, fly_from, days_range, price_to, adults):
+        """
+        Prepare data to the flight search and search for a flight.
+        :param city: city of arrival
+        :param fly_to: iata code city of arrival
+        :param fly_from: iata code cifty of departure
+        :param days_range: days to search flights in counted from today
+        :param price_to: max price
+        :param adults: number of adults
+        :return: found flight
+        """
+
+        d = DateData(int(days_range))
+        date_from = d.today_date
+        date_to = d.next_date
+        # print("Next_date:", next_date)
+
+        # checks flights
+        found_flight, status_code = FlightSearch().check_flights(fly_from=fly_from, fly_to=fly_to, date_from=date_from,
+                                                    date_to=date_to, price_to=price_to, adults=adults)
+
+        print("Results loc flight_data.py filter_from_user_input():", found_flight)
+        return found_flight, status_code
