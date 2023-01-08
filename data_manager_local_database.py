d449b566fbb359cf7aa9997b3b7c9b710cdadddb
Kami32l <79849248+Kami32l@users.noreply.github.com>
2023-01-08 02:28:18 +0100

    Update data_manager_local_database.py

--- data_manager_local_database.py
+++ data_manager_local_database.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 class DatabaseManager:
     """
-    This class is responsible for reading from and writing to database file with info about flights.
+    This class is responsible for reading from and writing to the database file with info about user and their flights.
     """
 
     def __init__(self):
-        self.file = self.read_file()
-        pass
+        self.file_name = 'database.tegst'
+        self.read_file()
 
     def read_file(self):
         """
@@ -16,22 +16,23 @@ class DatabaseManager:
         :return file: data from the database
         """
 
-        file = pd.read_csv('database.txt', delimiter=';')
-        # print(file)
-
-        return file
+        try:
+            self.file = pd.read_csv(self.file_name, delimiter=';')
+        except FileNotFoundError:
+            self.file = self.create_file()
+            self.save_data_to_file()
 
     def save_data_to_file(self):
         """
         Writes the pandas frame to the database file.
         """
-        self.file.to_csv('database.txt', sep=';', decimal=',', index=False)
+        self.file.to_csv(self.file_name, sep=';', decimal=',', index=False)
 
     def update_file(self, email, password, city, iata, maxprice, dep_city, dep_iata, people, days_range, next_date):
         """
         Adds user data to the pandas frame.
         :param email: email address ("email@gmail.com")
-        :param password: password ("pssstisas3cr3tphras3")
+        :param password: password already hashed ("pssstisas3cr3tphras3")
         :param city: airport city name ("Paris")
         :param iata: airport IATA code ("PAR")
         :param maxprice: maximum price you want to spend on flight, for example: 25
@@ -41,18 +42,41 @@ class DatabaseManager:
         :param days_range: number of days to search in
         :param next_date: end date of searching
         """
-        frame = {'Email': email, 'Password': password, 'City': city, 'IATA': iata, 'MaxPrice': maxprice, 'Dep_city': dep_city,
-                 'Dep_IATA': dep_iata, 'People': people, 'Days_range': days_range, 'Next_date': next_date}
+        frame = {'Email': email, 'Password': password, 'City': city, 'IATA': iata, 'MaxPrice': maxprice,
+                 'Dep_city': dep_city, 'Dep_IATA': dep_iata, 'People': people,
+                 'Days_range': days_range, 'Next_date': next_date}
         # self.file.append(frame, ignore_index=True)
 
         self.file = pd.concat([self.file, pd.DataFrame.from_records([frame])])
 
-    def search_database(self, key, word):
+    def search_database(self, key, word, key2, word2):
         """
-        Searches database for a record.
+        Searches database for a record. Two set of a key and a word. If key == word returns all rows matching
+        from DataFrame.
+        :param key: column name ('email')
+        :param key2: column name ('password')
+        :param word: what to look for in the column ('email@gmail.com')
+        :param word2: what to look for in the column ('password')
+        :return: all rows matching from the database
+        """
+
+        # This line of code will return all
+        # rows which satisfies both the conditions
+        # ie value of age == 35 and value of age == 40
+
+        return self.file[(self.file[f"{key}"] == f'{word}') & (self.file[f"{key2}"] == f'{word2}')]
+
+    def search_database_one(self, key, word):
+        """
+        Searches database for a record. One key and one word. If key == word returns all rows matching from DataFrame.
         :param key: column name ('email')
         :param word: what to look for in the column ('email@gmail.com')
-        :return: all data for the record from the database
+        :return: all rows matching from the database
         """
-        return self.file.loc[self.file[f'{key}'] == f'{word}']
 
+        return self.file.loc[self.file[f"{key}"] == f'{word}']
+
+    def create_file(self):
+        new_file = pd.DataFrame(columns=['Email', 'Password', 'City', 'IATA', 'MaxPrice', 'Dep_city', 'Dep_IATA',
+                                         'People', 'Days_range', 'Next_date'])
+        return new_file
