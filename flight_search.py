7ddc8d5d6569cfea9d14675a0449228663771c08
Kami32l <79849248+Kami32l@users.noreply.github.com>
2023-01-08 02:30:46 +0100

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -8,7 +8,7 @@ KIWI_API_KEY = os.getenv("KIWI_API_KEY")
 
 class FlightSearch:
     """
-    This class is responsible for talking to the KiwiCo Flight Search API.
+    This class is responsible for talking to the Kiwi Flight Search API.
     documentation - https://tequila.kiwi.com/docs/tequila_api/search_api
     """
     def __init__(self):
@@ -29,22 +29,30 @@ class FlightSearch:
             "location_types": "city",
         }
         endpoint = "https://tequila-api.kiwi.com/locations/query"
-        r = requests.get(url=endpoint, params=parameters, headers=headers)
-        print("status code get_location_code:", r.status_code)
-        data = r.json()
-        print(r.url)
-        print(data)
+        code = 0
         try:
-            code = data['locations'][0]['code']
-        except IndexError:
-            code = 0
+            r = requests.get(url=endpoint, params=parameters, headers=headers)
+            print("status code get_location_code:", r.status_code)
+            data = r.json()
+            print(r.url)
+            print(data)
+            status = 1
+            try:
+                code = data['locations'][0]['code']
+            except IndexError:
+                code = 0
+        except requests.exceptions.ConnectionError:
+            # TODO no connection window
+            print("no internet connection")
+            status = 0
+
         # print(code)
-        return code
+        return code, status
 
     def check_flights(self, fly_from, fly_to, date_from, date_to, price_to, adults=1, max_stopovers=3,
                       max_fly_duration=18, **kwargs):
         """
-        Checks for available flights using KiwiCo.
+        Checks for available flights using Kiwi.
         :param fly_from: city's of the departure IATA code
         :param fly_to: city's of the arrival IATA code
         :param date_from: starting date to look for a flight
@@ -53,7 +61,7 @@ class FlightSearch:
         :param adults: number of adults on the flight
         :param max_stopovers: maximal number of stops/changing the flights during travel to destination
         :param max_fly_duration: maximal duration of the flight (in hours)
-        :param kwargs: KiwiCO flight API provides more parameters than this method has specified more info in Kiwi Docs.
+        :param kwargs: Kiwi flight API provides more parameters than this method has specified more info in Kiwi Docs.
         :return: available flights
         """
 
@@ -74,14 +82,22 @@ class FlightSearch:
             "max_fly_duration": max_fly_duration,
         }
 
-
         # print(kwargs.items())
         for key, value in kwargs.items():
             parameters[key] = value
 
         endpoint = "https://tequila-api.kiwi.com/v2/search"
-        r = requests.get(url=endpoint, params=parameters, headers=headers)
-        data = r.json()
+
+        try:
+            r = requests.get(url=endpoint, params=parameters, headers=headers)
+            data = r.json()
+            status = 1
+        except requests.exceptions.ConnectionError:
+            # TODO no connection window
+            print("no internet connection")
+            data = []
+            status = 0
+
         # print(r.url)
         # print(data)
-        return data
+        return data, status
